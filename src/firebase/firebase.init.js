6dce042139c3490a04c30042e70262fa6b98d1e2
Tanvir <toukir.developer.bd@gmail.com>
2023-04-13 23:14:56 +0600

    first commit

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBx7j3kQa2YGhy6D2ljFZKJpwnxngvCMDA",
+    authDomain: "simple-firebase-62885.firebaseapp.com",
+    projectId: "simple-firebase-62885",
+    storageBucket: "simple-firebase-62885.appspot.com",
+    messagingSenderId: "147942608129",
+    appId: "1:147942608129:web:6d8dca815f0617b9475f00"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./Layout/Main";
+import Home from "./components/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
